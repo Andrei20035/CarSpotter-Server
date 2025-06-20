ab745ae3e57cdaf6733bf67355cf9a72a591c704
ANDREI MANUEL RUSU <andrei-manuel.rusu@s.unibuc.ro>
2025-06-20 16:36:05 +0300

    - Update tests to reflect V3__add__fullName_and_phoneNumber.sql migration.

--- server/src/main/kotlin/data/service/auth_credential/GoogleTokenVerifierImpl.kt
+++ server/src/main/kotlin/data/service/auth_credential/GoogleTokenVerifierImpl.kt
@@ -7,13 +7,16 @@ import com.google.api.client.json.gson.GsonFactory
 
 class GoogleTokenVerifierImpl : GoogleTokenVerifier {
     override fun verifyAndExtractSub(googleIdToken: String): String? {
+        val clientId = System.getenv("GOOGLE_CLIENT_ID")
+        println("ClientId: " + clientId)
         val transport = NetHttpTransport()
         val jsonFactory = GsonFactory.getDefaultInstance()
         val verifier = GoogleIdTokenVerifier.Builder(transport, jsonFactory)
             .setAudience(listOf(System.getenv("GOOGLE_CLIENT_ID")))
             .build()
-
+        println("Google id token: " + googleIdToken)
         val idToken: GoogleIdToken? = verifier.verify(googleIdToken)
+        println("Id token: " + idToken)
         return idToken?.payload?.subject
     }
 }
--- server/src/main/kotlin/routes/AuthRoutes.kt
+++ server/src/main/kotlin/routes/AuthRoutes.kt
@@ -14,6 +14,7 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
+import kotlin.math.log
 
 fun Route.authRoutes() {
     val authCredentialService: IAuthCredentialService by application.inject()
