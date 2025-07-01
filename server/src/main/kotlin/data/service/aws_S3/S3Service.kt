5aea5f9d22f4be30201c23b942c67a6e59285e9e
ANDREI MANUEL RUSU <andrei-manuel.rusu@s.unibuc.ro>
2025-07-01 20:10:27 +0300

    - Populate the db with car data.

--- server/src/main/kotlin/data/service/aws_S3/S3Service.kt
+++ server/src/main/kotlin/data/service/aws_S3/S3Service.kt
@@ -1,32 +1,37 @@
 package com.carspotter.data.service.aws_S3
 
-import org.gradle.internal.impldep.com.amazonaws.HttpMethod
-import org.gradle.internal.impldep.com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import org.gradle.internal.impldep.com.amazonaws.services.s3.AmazonS3
-import org.gradle.internal.impldep.com.amazonaws.services.s3.AmazonS3ClientBuilder
-import org.gradle.internal.impldep.com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.net.URL
-import java.util.Date
+import java.time.Duration
 
 class S3Service(
     private val bucketName: String,
     private val region: String,
 ): IStorageService {
-    private val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard()
-        .withRegion(region)
-        .withCredentials(DefaultAWSCredentialsProviderChain())
+    private val presigner = S3Presigner.builder()
+        .region(Region.of(region))
+        .credentialsProvider(DefaultCredentialsProvider.create())
         .build()
 
     override fun getPresignedUploadUrl(imageName: String): URL {
-        val expiration = Date(System.currentTimeMillis() + 15 * 60 * 1000) // 15 minutes
-        val request = GeneratePresignedUrlRequest(bucketName, imageName)
-            .withMethod(HttpMethod.PUT)
-            .withExpiration(expiration)
-        return s3Client.generatePresignedUrl(request)
+        val putObjectRequest = PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(imageName)
+            .build()
+
+        val presignRequest = PutObjectPresignRequest.builder()
+            .signatureDuration(Duration.ofMinutes(15))
+            .putObjectRequest(putObjectRequest)
+            .build()
+
+        return presigner.presignPutObject(presignRequest).url()
     }
 
     override fun getPublicImageUrl(imageName: String): String {
         return "https://$bucketName.s3.$region.amazonaws.com/$imageName"
     }
-
 }
